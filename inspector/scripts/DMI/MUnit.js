041e8acc3330223418af067b7babccaa36dcc42a
larzsp <larzsp@b642ac4a-e22c-bef7-dc4b-de626907f537>
2013-12-16 18:17:54 +0000

    Fix magic sorting.

--- inspector/scripts/DMI/MUnit.js
+++ inspector/scripts/DMI/MUnit.js
@@ -849,7 +849,7 @@ MUnit.CGrid = Utils.Class( DMI.CGrid, function() {
 				     .length==0 ) 
 				{
 					//add icon if not exists yet
-					$(".slick-header-column[title=Magic]")
+					$(".slick-header-column[id*=listed_mpath]")
 					.append('<img id="unitboosterordericon" alt="'+L+'" class="pathicon Path_'+L+'" src="images/magicicons/Path_'+L+'.png" />')
 					.find(".slick-sort-indicator").css('visibility','hidden');
 				}
